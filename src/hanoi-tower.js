eab46f322284f7e145c4127ba4f1cef4dbce1d15
Aliaksei Trunou <trunof@gmail.com>
2020-09-26 02:12:41 +0300

    feat: add  solution: season + hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  var actionCounts = Math.pow(2, disksNumber) - 1;
+  var sec = Math.floor(((actionCounts / turnsSpeed) * 3600));
+  return {turns: actionCounts, seconds: sec};
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date  || date - new Date() === 0) {
+    return 'Unable to determine the time of year!';
+  } else if (!(date instanceof Date) && isNaN(date.getMonth)){
+    throw new Error('Error');
+  }
+  
+  var i = date.getMonth();
+  if (i === 1 || i === 0 || i === 11){
+    return 'winter';
+  } else if (i === 2 || i === 3 || i === 4){
+    return 'spring';
+  } else if (i === 5 || i === 6 || i === 7){
+    return 'summer';
+  } else if (i === 8 || i === 9 || i === 10){
+    return 'autumn';
+  }
+
+}
