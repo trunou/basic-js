428338180b5b038066f937b85489420b15f98912
trunou <61742790+trunou@users.noreply.github.com>
2021-03-04 16:08:56 +0300

    update carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,15 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  var sactivity = '';
-  if(typeof(sampleActivity) === 'string' && (+sampleActivity) && (+sampleActivity) > 0 && (+sampleActivity) < 15 ){
-    sActivity = +sampleActivity;
-    return Math.ceil(Math.log(MODERN_ACTIVITY / sActivity) / (0.693 / HALF_LIFE_PERIOD));
-  } else{
+  if (typeof sampleActivity !== 'string' || parseFloat(sampleActivity) !== NaN)
     return false;
-  }
-  
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD))
 };
