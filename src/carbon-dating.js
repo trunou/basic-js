fe24883113cbb75b309a24d5eceb58b671c8ee88
trunou <61742790+trunou@users.noreply.github.com>
2021-03-07 16:56:24 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,10 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== 'string' || parseFloat(sampleActivity) !== NaN)
+  if(!sampleActivity || typeof sampleActivity !== "string" || MODERN_ACTIVITY < sampleActivity || sampleActivity < 0) {
     return false;
-  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD))
+  }
+  const act = parseFloat(sampleActivity);
+  return act ? Math.ceil(((Math.log(MODERN_ACTIVITY / act)) / (Math.log(2)) * HALF_LIFE_PERIOD)) : false;
 };
